b31b4444c3a7abd32f9bb407ad3a1cbb892f5782
Eugene <eugen.rabii@gmail.com>
2020-06-29 13:50:12 -0400

    added interesting example

---
+++ src/jmh/java/com/github/er/mapper/InterestingExampleTest.java
@@ -0,0 +1,84 @@
+package com.github.er.mapper;
+
+import com.github.er.mapper.examples.interesting.In;
+import com.github.er.mapper.examples.interesting.Mappers;
+import com.github.er.mapper.examples.interesting.Out;
+import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
+import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
+import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.BenchmarkMode;
+import org.openjdk.jmh.annotations.Fork;
+import org.openjdk.jmh.annotations.Level;
+import org.openjdk.jmh.annotations.Mode;
+import org.openjdk.jmh.annotations.OutputTimeUnit;
+import org.openjdk.jmh.annotations.Scope;
+import org.openjdk.jmh.annotations.Setup;
+import org.openjdk.jmh.annotations.State;
+import org.openjdk.jmh.annotations.Warmup;
+import org.openjdk.jmh.results.format.ResultFormatType;
+import org.openjdk.jmh.runner.Runner;
+import org.openjdk.jmh.runner.options.Options;
+import org.openjdk.jmh.runner.options.OptionsBuilder;
+
+@OutputTimeUnit(TimeUnit.NANOSECONDS)
+@Warmup(iterations = 10, time = 10)
+@State(Scope.Benchmark)
+@BenchmarkMode(Mode.AverageTime)
+public class InterestingExampleTest {
+
+    public static void runInteresting(String version) throws Exception {
+        Options opt = new OptionsBuilder()
+            .include(InterestingExampleTest.class.getSimpleName())
+            .resultFormat(ResultFormatType.JSON)
+            .result("interesting-bench-" + version + ".json")
+            .jvmArgs("-Xmx10g", "-Xms10g")
+            .build();
+
+        new Runner(opt).run();
+    }
+
+
+    public In<Long> in;
+
+    @Setup(value = Level.Invocation)
+    public void setUp() {
+
+        // I don't care about the contents of these
+        String userAge = ThreadLocalRandom.current().nextInt() + "";
+        String color = ThreadLocalRandom.current().ints().limit(5).mapToObj(String::valueOf).collect(Collectors.joining());
+        long l = ThreadLocalRandom.current().nextLong();
+
+        in = new In<Long>()
+            .setList(List.of(l))
+            .setUserAge(userAge)
+            .setColor(color);
+    }
+
+    // --------------------------------------------------------------------------------------
+
+    @Benchmark
+    @Fork(value = 1)
+    public Out<Integer> direct() {
+        return Mappers.directMapper().apply(in);
+    }
+
+    @Benchmark
+    @Fork(value = 1)
+    public Out<Integer> mapper() {
+        return Mappers.mapper().apply(in);
+    }
+
+    @Benchmark
+    @Fork(value = 1)
+    public Out<Integer> mapStruct() {
+        return Mappers.mapStructMapper().apply(in);
+    }
+
+    @Benchmark
+    @Fork(value = 1)
+    public Out<Integer> datus() {
+        return Mappers.datusMapper().apply(in);
+    }
+}
---
+++ src/jmh/java/com/github/er/mapper/RunJMHTests.java
@@ -0,0 +1,17 @@
+package com.github.er.mapper;
+
+import java.util.stream.IntStream;
+
+public class RunJMHTests {
+
+    public static void main(String[] args) throws Exception {
+        int i = IntStream.range(0, args.length)
+                         .filter(x -> "version".equals(args[x]))
+                         .findFirst()
+                         .orElseThrow(() -> new RuntimeException("version not provided"));
+
+        SimpleExampleTest.runSimple(args[i + 1]);
+        InterestingExampleTest.runInteresting(args[i + 1]);
+    }
+
+}
